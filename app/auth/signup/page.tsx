d765ff92ea5f7a38b73711594867c8396a1ab5fd
aslich86 <iwan.aslich@yahoo.com>
2025-06-11 18:50:23 +0700

    ganti bahasa

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -88,7 +88,7 @@ export default function SignupPage() {
               <input
                 id="username"
                 type="text"
-                placeholder="Masukkan username Anda"
+                placeholder="input your username"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
                 required
@@ -100,7 +100,7 @@ export default function SignupPage() {
                 <input
                     id="email"
                     type="email"
-                    placeholder="nama@email.com"
+                    placeholder="name@email.com"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                     required
@@ -129,7 +129,7 @@ export default function SignupPage() {
                   <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                   <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                 </svg>
-              ) : 'Daftar Sekarang'}
+              ) : 'Sign Up Now'}
             </button>
           </form>
           
